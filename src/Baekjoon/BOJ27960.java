c19a207752b5b83c1167c3b85a88b425f0e7a19e
USER <chldnrwo777@naver.com>
2023-05-19 09:39:42 +0900

    tsunagu

--- src/Baekjoon/BOJ27960.java
+++ src/Baekjoon/BOJ27960.java
@@ -1,9 +1,9 @@
 
-package A;
+package Baekjoon;
 
 import java.util.Scanner;
 
-class Main {
+class BOJ27960 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
 
