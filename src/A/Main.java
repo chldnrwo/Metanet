15624fb8ad01155d86b43c8c6a73968251f278f2
USER <chldnrwo777@naver.com>
2023-05-19 17:59:04 +0900

    tsunagu

--- src/A/Main.java
+++ src/A/Main.java
@@ -1,57 +1,75 @@
-
 package A;
 
-import java.util.Scanner;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
-class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+
+  private static String callChatGPT(String prompt) {
+    try {
+      URL url = new URL("https://api.openai.com/v1/completions");
+      HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+      httpURLConnection.setRequestMethod("POST");
+      httpURLConnection.setRequestProperty("Content-Type", "application/json; utf-8");
+      httpURLConnection.setRequestProperty("Authorization", "Bearer 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");
+
+      httpURLConnection.setDoOutput(true);
+
+      String input = "{\"model\": \"text-davinci-003\", \"prompt\": \"" + prompt + "\",\"max_tokens\" : 1024}";
+
+      try (OutputStream os = httpURLConnection.getOutputStream()) {
+        byte[] inputBytes = input.getBytes("utf-8");
+        os.write(inputBytes, 0, inputBytes.length);
+      }
+
+      int resCode = httpURLConnection.getResponseCode();
+      System.out.println("resCode:" + resCode);
+
+      try (BufferedReader br = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(), "utf-8"))) {
+        StringBuilder res = new StringBuilder();
+        String resLine = null;
+
+        while ((resLine = br.readLine()) != null) {
+          res.append(resLine.trim());
+        }
+
+        return (res.toString());
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return "";
+  }
+
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+    String prompt = "골든 리트리버의 역사에 대해 한 페이지 분량으로 설명해줘";
 
-    int a = in.nextInt();
-    int b = in.nextInt();
+    String res = callChatGPT(prompt);
 
-    String ba = Integer.toBinaryString(a);
-    String bb = Integer.toBinaryString(b);
-    //System.out.println(ba);
-    //System.out.println(bb);
-    String s = "";
-    int alen = ba.length();
-    int blen = bb.length();
-    int len = Math.max(ba.length(), bb.length());
+    JSONParser jsonParser = new JSONParser();
 
-    if(alen < blen){
-      for(int i = 0; i < len-alen; i++){
-        ba = "0"+ba;
+    try {
+      if (!res.isEmpty()) {
+        Object obj = jsonParser.parse(res);
+        JSONObject jsonObject = (JSONObject) obj;
+        JSONArray choices = (JSONArray) jsonObject.get("choices");
+        JSONObject choice = (JSONObject) choices.get(0);
+        String text = (String) choice.get("text");
+        System.out.println(res);
+        System.out.println(text);
+      } else {
+        System.out.println("Response is empty.");
       }
-    }else{
-      for(int i = 0; i < len-blen; i++){
-        bb = "0"+bb;
-      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    //System.out.println(ba);
-    //System.out.println(bb);
-
-    for(int i = 0;i<len;i++){
-      if(ba.charAt(i) != bb.charAt(i)){
-        s += "1";
-      }else{
-        s += "0";
-      }
-    }
-
-
-    int sum = 0;
-    for(int i = 0; i < s.length(); i++){
-      if(s.charAt(i) == '1'){
-        sum += Math.pow(2, len -1 - i);
-      }
-    }
-
-    System.out.println(sum);
   }
 }
-/*
-
-
-
- */
