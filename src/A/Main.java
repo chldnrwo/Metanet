f5fb197326a371cef466d4426a208620018db49f
USER <chldnrwo777@naver.com>
2023-06-02 17:26:01 +0900

    CUID

--- src/A/Main.java
+++ src/A/Main.java
@@ -1,18 +1,36 @@
 package A;
 
-import java.util.Scanner;
+import java.util.Random;
 
-class Main {
+public class Main {
+
+  private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789";
+  private static final Random RANDOM = new Random();
+
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+    System.out.println(generate());
+  }
+  public static String generate() {
+    return "c" + encodeTimestamp(System.currentTimeMillis()) + generateRandomString(10);
+  }
 
-    int a = in.nextInt();
-    int b = in.nextInt();
-    System.out.println(a+b);
+  private static String encodeTimestamp(long timestamp) {
+    StringBuilder result = new StringBuilder();
+    while (timestamp > 0) {
+      result.append(ALPHABET.charAt((int) (timestamp % ALPHABET.length())));
+      timestamp /= ALPHABET.length();
+    }
+    while (result.length() < 8) {
+      result.append('0');
+    }
+    return result.toString();
+  }
+
+  private static String generateRandomString(int length) {
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < length; i++) {
+      result.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+    }
+    return result.toString();
   }
 }
-/*
-
-
-
- */
