9fcbde265cdfaca4481ff629ac0783c684b005bd
USER <chldnrwo777@naver.com>
2023-06-05 09:15:46 +0900

    ddd

---
+++ src/A/PrimaryGenerator.java
@@ -0,0 +1,36 @@
+package A;
+
+import java.util.Random;
+
+public class PrimaryGenerator {
+
+  private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789";
+  private static final Random RANDOM = new Random();
+
+  public static void main(String[] args) {
+    System.out.println(generate());
+  }
+  public static String generate() {
+    return "c" + encodeTimestamp(System.currentTimeMillis()) + generateRandomString(10);
+  }
+
+  private static String encodeTimestamp(long timestamp) {
+    StringBuilder result = new StringBuilder();
+    while (timestamp > 0) {
+      result.append(ALPHABET.charAt((int) (timestamp % ALPHABET.length())));
+      timestamp /= ALPHABET.length();
+    }
+    while (result.length() < 8) {
+      result.append('0');
+    }
+    return result.toString();
+  }
+
+  private static String generateRandomString(int length) {
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < length; i++) {
+      result.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+    }
+    return result.toString();
+  }
+}
--- src/Baekjoon/BOJ27649.java
+++ src/Baekjoon/BOJ27649.java
@@ -4,7 +4,7 @@ package Baekjoon;
 //import java.util.List;
 //import java.util.Scanner;
 //
-//class Main {
+//class BOJ2920 {
 //  public static void main(String[] args) {
 //    Scanner in = new Scanner(System.in);
 //    List<String> list = new ArrayList<>();
